b98c098f0baf6285480d9bee3c3aba8d381efec0
Anthony Fu <anthonyfu117@hotmail.com>
2024-05-14 03:36:05 +0200

    feat: store stack string instead of error object

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import type { StackFrameLite } from 'error-stack-parser-es/lite'
-import { parse } from 'error-stack-parser-es/lite'
+import { parseStack } from 'error-stack-parser-es/lite'
 
 // @ts-expect-error missing globalThis type
-let map = globalThis.__TRACE_RECORD_MAP__
+let map: WeakMap<any, string> = globalThis.__TRACE_RECORD_MAP__
 if (!map) {
-  map = new WeakMap<any, Error>()
+  map = new WeakMap<any, string>()
   Object.defineProperty(globalThis, '__TRACE_RECORD_MAP__', {
     value: map,
     writable: false,
@@ -14,18 +14,19 @@ if (!map) {
 
 export function record<T extends WeakKey>(arg: T) {
   // eslint-disable-next-line unicorn/error-message
-  map.set(arg, new Error())
+  const stack = new Error().stack
+  if (stack)
+    map.set(arg, stack)
   return arg
 }
 
 export function getTraceRaw<T extends WeakKey>(_arg: T): string | undefined {
-  return map.get(_arg)?.stack
+  return map.get(_arg)
 }
 
 export function getTrace<T extends WeakKey>(_arg: T): StackFrameLite[] | undefined {
   const stack = map.get(_arg)
   return stack
-    ? parse(stack)
-      .slice(1)
+    ? parseStack(stack).slice(1)
     : undefined
 }
